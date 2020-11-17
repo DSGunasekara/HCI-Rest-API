98776a81ba2c8ca87fc789258aac53d9cf29582d
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-11-17 19:37:51 +0530

    user route updated

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,13 @@ const { model } = require('../models/User');
 const User = require('../models/User');
 
 router.get('/', async(req, res)=>{
-    return res.status(200).send('Hello')
+    try {
+        const email = 'adsgunasekara18@gmail.com'
+        const user = await User.findOne({ email })
+        return res.status(200).send(user)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
 })
 
 router.post('/register', async(req, res)=>{
@@ -45,8 +51,9 @@ router.post('/login', async(req, res)=>{
 
 router.patch('/withdraw', async(req, res)=>{
     try {
-        const { email } = req.body;
-        const updatedUser = await User.updateOne({ email }, req.body)
+        const email = 'adsgunasekara18@gmail.com' 
+        console.log(req.body);
+        await User.updateOne({ email }, req.body)
         return res.status(200).send("user Updated")
 
     } catch (error) {
