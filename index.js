bacff54edcb2cafa176964303638dba86a812752
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-11-17 13:02:29 +0530

    withdraw added

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+const connectDB = require('./config/db')
+
+const userRoute = require('./routes/user')
 
 const app = express()
+
+connectDB().then(
+    (res)=> console.log('MongoDB Connected....'),
+    (error)=> console.log(error)
+)
 
 app.use(cors())
 app.use(express.json({extended: false}))
 
 app.get('/', (req, res)=> res.status(200).send('API is running'))
+app.use('/api/user', userRoute)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`App is running on Port ${PORT}`));
---
+++ routes/user.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt')
+
+const { model } = require('../models/User');
+const User = require('../models/User');
+
+router.get('/', async(req, res)=>{
+    return res.status(200).send('Hello')
+})
+
+router.post('/register', async(req, res)=>{
+    try {
+
+        const user = new User({...req.body})
+
+        console.log(user);
+        const pin = await bcrypt.hash(user.pin, 10);
+
+        user.pin = pin;
+        console.log(user.pin);
+        await user.save();
+        return res.status(200).send(user)
+
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+router.post('/login', async(req, res)=>{
+    try {
+        const { email, pin } = req.body;
+
+        const user = await User.findOne({ email })
+        if(!user) return res.status(404).send("User not found")
+
+        const isMatch = await bcrypt.compare(pin, user.pin);
+        if(!isMatch) return res.status(400).send("Invalid credentials")
+
+        return res.status(200).send(user)
+
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+router.patch('/withdraw', async(req, res)=>{
+    try {
+        const { email } = req.body;
+        const updatedUser = await User.updateOne({ email }, req.body)
+        return res.status(200).send("user Updated")
+
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+module.exports = router;
